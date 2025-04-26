3f1a8171ad3563670ffc08231511b94f1619fe48
Alialaa122 <133665586+Alialaa122@users.noreply.github.com>
2025-04-26 15:50:18 +0300

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -14,24 +14,24 @@
     # print("Congartulations!")
 
 
-# import random
+import random
 
-# random_num=random.randint(1,100)
-# while True:
+random_num=random.randint(1,100)
+while True:
 
-#     try:
-#         guess=int(input("Enter A Number (Between 1 and 100): "))
+    try:
+        guess=int(input("Enter A Number (Between 1 and 100): "))
         
-#         if guess > random_num:
-#             print("too high")
-#         elif guess < random_num:
-#             print("too low")
-#         else:
-#             print("Congratulations! You Guessed The right Number")
-#             break
+        if guess > random_num:
+            print("too high")
+        elif guess < random_num:
+            print("too low")
+        else:
+            print("Congratulations! You Guessed The right Number")
+            break
 
-#     except ValueError:
-#         print("Enter A Valid Number")
+    except ValueError:
+        print("Enter A Valid Number")
 
 
 
@@ -40,42 +40,44 @@
 
 # Optional Enhancements
 
-# import random
+import random
 
-# try:
-#     minimum=int(input("Enter The minimum range: "))
-#     maximum=int(input("Enter The maximum range: "))
-#     random_num=random.randint(minimum,maximum)
-# except ValueError:
-#     print("Plese Enter A valid range")
-
-
-# attempts=0
-# best_score=0
-
-# while attempts != 5:
-
-#     try:
+try:
+    minimum=int(input("Enter The minimum range: "))
+    maximum=int(input("Enter The maximum range: "))
+    random_num=random.randint(minimum,maximum)
+except ValueError:
+    print("Plese Enter A valid range")
 
 
-#         guess=int(input(f"Enter A Number (Between {minimum} and {maximum}): "))
+attempts=0
+best_score=0
+
+while attempts != 5:
+
+    try:
+
+
+        guess=int(input(f"Enter A Number (Between {minimum} and {maximum}): "))
         
-#         if guess > random_num:
-#             print("too high")
-#             attempts+=1
-#         elif guess < random_num:
-#             print("too low")
-#             attempts+=1
-#         else:
-#             print("Congratulations! You Guessed The right Number")
-#             print(f"The Best Score is: {attempts} attempts.")
-#             break
+        if guess > random_num:
+            print("too high")
+            attempts+=1
+        elif guess < random_num:
+            print("too low")
+            attempts+=1
+        else:
+            print("Congratulations! You Guessed The right Number")
+            print(f"The Best Score is: {attempts} attempts.")
+            break
 
-#     except ValueError:
-#         print("Enter A Valid Number")
+    except ValueError:
+        print("Enter A Valid Number")
 
-# else:
-#     print("Sorry You have finished Your 5 Attempts")
-#     print(f"The right Number is {random_num} ")
+else:
+    print("Sorry You have finished Your 5 Attempts")
+    print(f"The right Number is {random_num} ")
+
+
 
 
